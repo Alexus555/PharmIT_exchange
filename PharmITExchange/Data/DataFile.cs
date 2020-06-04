272654e1f0f724a0e13c699a2b037af66ab8b8bb
Alexus555 <alexus555@yandex.ru>
2020-06-04 17:36:37 +0600

    Add 'Month' prop in class DataFile

--- PharmITExchange/Data/DataFile.cs
+++ PharmITExchange/Data/DataFile.cs
@@ -6,6 +6,7 @@ namespace PharmITExchange
     public class DataFile
     {
         public DateTime Date { get; set; }
+        public DateTime Month { get; set; }
         public int Count { get; set; }
         public List<PageData> Pages { get; set; }
     }
